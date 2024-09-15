0312a25aa556bc1d916836874b9fb680472f5cb6
Kriptel <steveboommcpe@gmail.com>
2024-09-15 14:10:31 +0300

    Update TouchMacro.hx

--- rulescript/macro/TouchMacro.hx
+++ rulescript/macro/TouchMacro.hx
@@ -3,16 +3,19 @@ package rulescript.macro;
 import haxe.macro.Context;
 import haxe.macro.Expr.Field;
 
-class TouchMacro {
-	public static macro function touch():Array<Field> {
-		var __touch = Std.string(Date.now().getTime());
-	
+class TouchMacro
+{
+	public static macro function touch():Array<Field>
+	{
+		var __touch = Std.string(Math.random() * 0x7FFFFFFF);
+
 		var fields = Context.getBuildFields();
 		fields.push({
 			name: '__touch',
 			pos: Context.currentPos(),
 			access: [APrivate, AStatic, AInline],
-			kind: FVar(macro : String, macro $v{__touch})
+			kind: FVar(macro :String, macro $v{__touch}),
+			meta: [{pos: Context.currentPos(), name: ':noCompletion'}]
 		});
 		return fields;
 	}
