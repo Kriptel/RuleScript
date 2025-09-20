23ea7bdc517468f5a2224619cd061320109c3dcf
glowsoony <84847356+glowsoony@users.noreply.github.com>
2025-09-20 16:45:43 -0400

    Allow resetting variables in RuleScriptInterpAccess

--- rulescript/interps/RuleScriptInterp.hx
+++ rulescript/interps/RuleScriptInterp.hx
@@ -893,6 +893,11 @@ class RuleScriptInterpAccess extends RuleScriptAccess
 	{
 		return interp.variables = newVariables;
 	}
+	
+	override function resetVariables():Void
+	{
+		interp.resetVariables();
+	}
 
 	override function variableExists(name:String):Bool
 	{
