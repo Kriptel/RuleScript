dcd720bb63b8aee942eb0db7307cb122749478cd
HackDev <80620538+Hackx2@users.noreply.github.com>
2025-12-04 23:04:33 +0000

    enum abstracts

--- rulescript/types/context/EVariableModifiers.hx
+++ rulescript/types/context/EVariableModifiers.hx
@@ -1,6 +1,6 @@
 package rulescript.types.context;
 
-enum EVariableModifiers {
-	PUBLIC;
-	STATIC;
+enum abstract EVariableModifiers(Int) {
+	var PUBLIC:EVariableModifiers = 0;
+	var STATIC:EVariableModifiers = 1;
 }
