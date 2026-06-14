dcad9ec153fcbe353517cee1972fe74b9a89967c
Alejo GD Official <102831994+AlejoGDOfficial@users.noreply.github.com>
2026-06-13 22:17:51 -0500

    Refactor assign function to improve variable handling

--- rulescript/interps/RuleScriptInterp.hx
+++ rulescript/interps/RuleScriptInterp.hx
@@ -115,18 +115,30 @@ class RuleScriptInterp extends hscript.Interp implements IInterp
 	override function assign(e1:Expr, e2:Expr):Dynamic
 	{
 		var v = expr(e2);
+
 		switch (hscript.Tools.expr(e1))
 		{
 			case EIdent(id):
 				var l = locals.get(id);
+
 				if (l == null)
 					setVar(id, v);
 				else
 				{
 					if (l.r is Property)
 						cast(l.r, Property).value = v;
-					else
+					else {
 						l.r = v;
+
+						if (context != null)
+						{
+							if (context.staticVariables.exists(id))
+								context.staticVariables.set(id, v);
+
+							if (context.publicVariables.exists(id))
+								context.publicVariables.set(id, v);
+						}
+					}
 				}
 			case EField(e, f):
 				v = set(expr(e), f, v);
